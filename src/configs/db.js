ed0858a55007a7f625523f10d5abdda9394ca292
Scythrine 05 | Rohan Murmu <scythrine05@gmail.com>
2024-12-24 18:41:10 +0530

    Fixed Mongo-bug: 1

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB Connected");
   } catch (err) {
     throw err;
