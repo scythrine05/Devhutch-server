f07d5fa25d14e8b54dff65d1c0c89bcceddbbfe9
Scythrine 05 | Rohan Murmu <scythrine05@gmail.com>
2025-01-02 01:52:49 +0530

    Added httpOnly cookies

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -2,20 +2,27 @@ require("dotenv").config();
 const admin = require("firebase-admin");
 
 const verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1] || req.cookies.token;
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const token = authHeader.split(" ")[1];
-
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
     req.user = decodedToken;
+
+    if (req.headers.authorization?.split(" ")[1])
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 15 * 60 * 1000,
+      });
+
     next();
   } catch (error) {
-    console.error("Token verification failed:", error);
     return res.status(401).json({ message: "Unauthorized" });
   }
 };
